5275fd74acb03ef80341f41cad251ded6e04a3a6
Denis Ogun <do1303@my.bristol.ac.uk>
2015-02-02 13:06:17 +0000

    Start using RFDuino

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var argv = require('yargs')
 					.describe('v', 'Be verbose about BLE events (not the advertising packets)')
 					.argv;
 // Constants
-var userServiceUUID = "eb03a1e1663c414ea8126f8a94cdfb35";
-var userCharacteristicUUID = "4a3c42c4de114357bfbef40d612c1ffc";
-var nonceUUID = "0faba94acf64400289b66ce5088f18cb";
-var keyUUID = "b09ef2475b9241678b9545d8193f7ea3";
+var userServiceUUID = "2220";
+var readCharacteristicUUID = "2221";
+var writeCharacteristicUUID = "2222";
+var disconnectCharacteristicUUID = "2223";
 
 // Variables
 var userKey = "";
@@ -17,6 +17,9 @@ var activePeripherals = {};
 var needsCheckingQueue = [];
 var isScanning = false;
 var locked = 0;
+
+var readChannel = null;
+var writeChannel = null;
 
 if (!Date.now) {
     Date.now = function() { return new Date().getTime(); }
@@ -30,7 +33,10 @@ setInterval(function(){
 
 	for (var peripheralKey in activePeripherals) {
 
-		if ( (activePeripherals[peripheralKey]["lastConnectionTime"] < (Date.now() - 15000)) && activePeripherals[peripheralKey]["lastConnectionTime"] > (Date.now() - 60000) ) {
+		// If we haven't seen a device in 15 seconds look for it again
+		// otherwise if it hasn't been seen in 60 seconds delete it
+		if ((activePeripherals[peripheralKey]["lastConnectionTime"] < (Date.now() - 15000)) 
+			&& activePeripherals[peripheralKey]["lastConnectionTime"] > (Date.now() - 60000)) {
 
 			// Is it already in the queue?
 			for (var i = 0; i < needsCheckingQueue.length; i++) {
@@ -38,9 +44,7 @@ setInterval(function(){
 			}
 
 			needsCheckingQueue.push([peripheralKey, activePeripherals[peripheralKey]])
-		}
-
-		else if (activePeripherals[peripheralKey]["lastConnectionTime"] < (Date.now() - 60000)) {
+		} else if (activePeripherals[peripheralKey]["lastConnectionTime"] < (Date.now() - 60000)) {
 			console.log("Deleting " + peripheralKey + " at " + Date.now());
 			delete activePeripherals[peripheralKey];
 			removePeripheralFromChecking(peripheralKey);
@@ -108,9 +112,62 @@ noble.on('scanStop', function() {
 	printBLEMessage('on -> scanStop');
 });
 
-noble.on('discover', function(peripheral) {
+var onServiceDiscoveredCallback = function(services) {
+	for (var serviceID in services) {
+		service = services[serviceID];
+		if (service["uuid"] === userServiceUUID) {
+			service.on('characteristicsDiscover', function(characteristics) {
+				for (var characteristicID in characteristics) {
+					characteristic = characteristics[characteristicID];
+					if (characteristic["uuid"] === readCharacteristicUUID) {
+					/*	characteristic.on('write', function() {
+							printBLEMessage('on -> characteristic write ');
+						});
+						
+						var nonceString = makeNonce();
+						var nonceValue = new Buffer(nonceString, "utf-8");
 
-	if (argv.p == true) console.log('on -> discover: ' + peripheral);
+						console.log("Sending Nonce " + nonceString);
+
+						characteristic.write(nonceValue, function(err) {
+							if (err) {
+								printBLEMessage("Error writing value " + err);
+							}
+						});*/
+						readChannel = characteristic;
+					} else if (characteristic["uuid"] === writeCharacteristicUUID) {
+					/*	characteristic.notify(true, function(err) {
+							if (err) {
+								printBLEMessage("Error subscribing to notification " + err);
+							}
+						});
+
+						characteristic.on('notify', function(state) {
+							printBLEMessage('on -> characteristic notify ' + state);
+							userKey = "";
+						});
+
+						characteristic.on('read', function(data, isNotification) {
+							if (data.toString('hex') == "04") {
+								console.log("Final user key: " + userKey);
+								locked = 0;
+								peripheral.disconnect();
+							} else {
+								userKey += data;
+							}
+						});		*/
+						writeChannel = characteristic;
+					}
+				}
+			});
+		service.discoverCharacteristics();
+		}
+	}
+};
+
+var onDeviceDiscoveredCallback = function(peripheral) {
+	if (argv.p == true) 
+		console.log('on -> discover: ' + peripheral);
 
 	if (locked != 0) {
 		return;
@@ -119,107 +176,32 @@ noble.on('discover', function(peripheral) {
 	}
 
 	// Only connect to a peripheral if it's not in activePeripherals or if it's in needsCheckingQueue 
-	if ( (activePeripherals[peripheral["uuid"]] == undefined) || doesPeripheralNeedChecking(peripheral["uuid"]) ) {
+	if ((activePeripherals[peripheral["uuid"]] == undefined) || doesPeripheralNeedChecking(peripheral["uuid"])) {
 	
 		peripheral.once('connect', function() {
 			printBLEMessage('on -> connect');
-			this.updateRssi();
+			this.discoverServices();
 		});
 
 		peripheral.once('disconnect', function() {
+			// Reset the channels
+			readChannel = null;
+			writeChannel = null;
 
-			for (var serviceID in peripheral["services"]) {
-				var service = peripheral["services"][serviceID]
-				if (service["uuid"] == userServiceUUID) {
-					for (var characteristicID in service["characteristics"]) {
-						characteristic = service["characteristics"][characteristicID];
-						if (characteristic["uuid"] === userCharacteristicUUID) {
-							//console.log("Deleting " + characteristic["_peripheralUuid"]);
-							//delete activePeripherals[characteristic["_peripheralUuid"]];
-						}
-					}
-				}
-			}
-			printBLEMessage('on -> disconnect');
-
-			printBLEMessage("Start scanning again")
+			printBLEMessage("Disconnected....Starting to scan again")
 			startScanning();
 		});
 
 		peripheral.once('rssiUpdate', function(rssi) {
 			printBLEMessage('on -> RSSI update ' + rssi);
-			this.discoverServices();
 		});
 
-		peripheral.once('servicesDiscover', function(services) {
-			for (var serviceID in services) {
-				service = services[serviceID];
-				if (service["uuid"] === userServiceUUID) {
-					service.on('characteristicsDiscover', function(characteristics) {
-						for (var characteristicID in characteristics) {
-							characteristic = characteristics[characteristicID];
-							if (characteristic["uuid"] === userCharacteristicUUID) {							
-								characteristic.on('read', function(data, isNotification) {
-									printBLEMessage('on -> characteristic read ' + data + ' ' + isNotification);
-
-									var peripheralData = {
-										name: data.toString(),
-										lastConnectionTime: Date.now()
-									}
-
-									console.log("Found new user: " + peripheralData.name);
-
-									activePeripherals[peripheral["uuid"]] = peripheralData;
-									removePeripheralFromChecking(peripheral["uuid"]);
-									console.log(activePeripherals)
-								});
-
-								characteristic.read();
-
-							} else if (characteristic["uuid"] === nonceUUID) {
-								characteristic.on('write', function() {
-									printBLEMessage('on -> characteristic write ');
-								});
-								var nonceString = makeNonce();
-								var nonceValue = new Buffer(nonceString, "utf-8");
-
-								console.log("Sending Nonce " + nonceString);
-
-								characteristic.write(nonceValue, function(err) {
-									if (err) {
-										printBLEMessage("Error writing value " + err);
-									}
-								});
-							} else if (characteristic["uuid"] === keyUUID) {
-								characteristic.notify(true, function(err) {
-									if (err) {
-										printBLEMessage("Error subscribing to notification " + err);
-									}
-								});
-
-								characteristic.on('notify', function(state) {
-									printBLEMessage('on -> characteristic notify ' + state);
-									userKey = "";
-								});
-
-								characteristic.on('read', function(data, isNotification) {
-									if (data.toString('hex') == "04") {
-										console.log("Final user key: " + userKey);
-										locked = 0;
-										peripheral.disconnect();
-									} else {
-										userKey += data;
-									}
-								});					
-							}
-						}
-					});
-					service.discoverCharacteristics();
-				}
-			}
-		});
+		peripheral.once('servicesDiscover', onServiceDiscoveredCallback);
 
 		stopScanning();
+
+		console.log("Found user with UUID: " + getUserUUID(peripheral));
+
 		peripheral.connect(function(err) {
 			if (err) {
 				console.log(err);
@@ -229,7 +211,10 @@ noble.on('discover', function(peripheral) {
 		locked = 0;
 		startScanning();
 	}
-});
+
+};
+
+noble.on('discover', onDeviceDiscoveredCallback);
 
 // http://stackoverflow.com/questions/1349404/generate-a-string-of-5-random-characters-in-javascript
 function makeNonce()
@@ -241,4 +226,9 @@ function makeNonce()
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return text;
+}
+
+function getUserUUID(peripheral) 
+{
+	return peripheral.advertisement.manufacturerData.slice(2).toString();
 }
