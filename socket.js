37c402e8d8c32f15506e3390a92989a4f3623593
Maciej Kumorek <mkumorek@gmail.com>
2015-05-13 11:19:21 +0100

    Nick removed from mock data

--- socket.js
+++ socket.js
@@ -25,7 +25,7 @@ function getMockData() {
 
 	// Some mock users
 	var users = [
-			{id: 'EA8F2A44', state: 'active' },
+			//{id: 'EA8F2A44', state: 'active' },
 			{id: 'ABC1231', state: 'active' },
 			{id: 'tester', state: 'active' },
 			{id: 'ABC1232', state: 'active' },
