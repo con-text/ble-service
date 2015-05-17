611c0fc8f402fe437475b631b9f376b410c5b291
Benji Barash <bbarash@gmail.com>
2015-05-17 16:19:38 +0100

    Adjusting timings

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ setInterval(function(){
 
 			var activePeripheral = bluetooth.activePeripherals[peripheralKey];
 
-			// If we last saw the peripheral over 15s ago, but less than a minute
-			if (activePeripheral.lastConnectionTime < (common.currentDate() - 15) &&
+			// If we last saw the peripheral over 20s ago, but less than a minute
+			if (activePeripheral.lastConnectionTime < (common.currentDate() - 20) &&
 				activePeripheral.lastConnectionTime > (common.currentDate() - 60)) {
 
 				// Is it already in the needsChecking queue?
