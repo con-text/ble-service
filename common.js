68f10ee3c136578da8376604fd2295e95f255f5f
Benji Barash <bbarash@gmail.com>
2015-05-17 16:03:22 +0100

    Reduced socket update delay

--- common.js
+++ common.js
@@ -9,7 +9,7 @@ var request = require('request');
 var keyMirror = require('keymirror');
 
 var useMockData = false;
-var updateInterval = 5000;
+var updateInterval = 2500;
 
 var messageCodes = keyMirror({activePeripherals: null, loginStatus: null});
 
